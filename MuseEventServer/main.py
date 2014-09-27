22cae5b26d77d87c1e3b939ede491d31ab3fe238
Jordan Pryde <jordan@pryde.me>
2014-09-27 04:55:33 -0400

    The modularized and generic version of what was hacked at Hack the North 2014. Incomplete!

---
+++ MuseEventServer/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2.7
+"""MuseIC EventServer Demo Runner"""
+import sys
+
+from utils import event_provider
+
+__author__ = "Jordan Pryde"
+__copyright__ = "Copyright 2014, Jordan Pryde"
+__credits__ = ["Jordan Pryde", "Shirley Miao", "Athul Paul Jacob",
+                    "Pritham"]
+__license__ = "MIT"
+__version__ = "0.2.0"
+__maintainer__ = "Jordan Pryde"
+__email__ = "jordan@pryde.me"
+__status__ = "experimental"
+
+if __name__ == '__main__':
+    # display help
+    if len(sys.argv) == 1 or sys.argv[1] in ("-h", "--help"):
+        sys.exit("Usage: " + sys.argv[0] + " port")
+
+    # require one argument (port number)
+    if len(sys.argv) < 2:
+        sys.exit("please specify a port or socket path")
+
+    msg_center = event_provider.EventProvider()
+    app = 
+    try:
+        app.run()
+    except KeyboardInterrupt:
+        del app
